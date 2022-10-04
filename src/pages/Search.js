75ceae4e422f241e09309d28d8e19373e48fbb34
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-10-04 18:34:48 -0400

    Conclui requisito N°6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import Header from '../components/Header';
 
+const num = 2;
+
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pesquisa: '',
+    };
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    const { pesquise } = this.state;
+    this.setState({
+      pesquisa: value,
+    });
+  };
+
   render() {
+    const { pesquisa } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <input
+          data-testid="search-artist-input"
           type="text"
-          placeholder="Procurar search page"
+          placeholder="Pesquisa Mesmo"
+          onChange={ this.handleChange }
         />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ (pesquisa.length < num) }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
