1fe07da75158e3ed89071983d463d0e0d0c3eb52
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-10-21 18:45:01 -0400

    Conclui requisito N°13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      algo: [],
+      carregando: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getUserMesmo();
+  }
+
+  getUserMesmo = () => {
+    getUser().then((response) => this.setState({ algo: response, carregando: false }));
+  };
+
   render() {
+    const { algo, carregando } = this.state;
+    console.log(algo);
     return (
       <div data-testid="page-profile">
         <Header />
+        {carregando ? <Loading /> : (
+          <div>
+            <div>
+              <p>{algo.name}</p>
+              <p>{algo.email}</p>
+              <img
+                src={ algo.image }
+                alt={ algo.name }
+                data-testid="profile-image"
+              />
+              <p>{algo.description}</p>
+              <div>
+                <Link to="/profile/edit">Editar perfil</Link>
+              </div>
+            </div>
+          </div>
+        )}
+
       </div>
     );
   }
